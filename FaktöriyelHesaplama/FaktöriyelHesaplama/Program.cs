7b8ada0b8d430d61af62d80344767832e769a3fa
Furkan Sarışın <furkansarisinn@gmail.com>
2024-02-24 19:35:32 +0300

    Faktöriyel Hesaplama

---
+++ FaktöriyelHesaplama/FaktöriyelHesaplama/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaktöriyelHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Faktöriyeli hesaplanacak sayıyı girin: ");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+            int faktöriyel = 1;
+            for (int i = 1; i <= sayı; i++)
+            {
+                faktöriyel *= i;
+            }
+            Console.WriteLine("Sayının faktöriyel değeri: " + faktöriyel);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FaktöriyelHesaplama/FaktöriyelHesaplama/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("FaktöriyelHesaplama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FaktöriyelHesaplama")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("db936f67-56c0-4b21-9f5c-7f638d513d06")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
