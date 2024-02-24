820496c44f921088beb02b83451fd6f0aa6c6160
Furkan Sarışın <furkansarisinn@gmail.com>
2024-02-24 20:32:44 +0300

    Asal Sayı Kontrol

---
+++ AsalSayıKontrol/AsalSayıKontrol/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AsalSayıKontrol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kontrol etmek istediğiniz sayıyı giriniz: ");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+            bool sonuc = true;
+
+            for (int i = 2; i <= sayı / 2; i++)
+            {
+                if (sayı % i == 0)
+                {
+                    sonuc = false;
+                    break; // Bölen bulunduğunda döngüden çık
+                }
+            }
+
+            if (sonuc && sayı >= 2)
+            {
+                Console.WriteLine("Asal");
+            }
+            else
+            {
+                Console.WriteLine("Asal Değil");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
