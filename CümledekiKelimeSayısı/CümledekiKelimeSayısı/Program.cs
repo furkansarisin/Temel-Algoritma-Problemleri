aef9b30620fddeda5421581dba7401894618da81
Furkan Sarışın <furkansarisinn@gmail.com>
2024-02-24 20:49:50 +0300

    Kelime sayıcı
    Cümleyi kelimelerine ayırır ve toplam kelime sayısını yazdırır, sadece tek boşluk ile ayrılan cümlelerde doğru sonucu vermektedir.

---
+++ CümledekiKelimeSayısı/CümledekiKelimeSayısı/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CümledekiKelimeSayısı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cümle yazınız!");
+            string cümle = Console.ReadLine();
+            string[] kelimeler = cümle.Split(' ');
+
+            Console.WriteLine($"Kelime sayısı: {kelimeler.Length}");
+
+            foreach (string kelime in kelimeler)
+            {
+                Console.WriteLine(kelime);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
