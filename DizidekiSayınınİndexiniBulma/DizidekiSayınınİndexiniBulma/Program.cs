18f59255d4a7c9f58565f2d52e0f968920489017
Furkan Sarışın <furkansarisinn@gmail.com>
2024-02-28 16:51:05 +0300

    Dizideki değerin index'ini bulma
    Binary Search algoritmasını kullanarak sayının dizi'deki index'ini bulma. Algoritma, ilk başta dizinin ortasındaki değeri kontrol eder, eşitse arama biter index'i bulmuşuz demektir.. Eğer ortadaki değer, kullanıcının girdiği sayıdan büyükse, arama ortadaki değer - 1 ile sıfır indeksi arasında devam eder. Eğer ortadaki değer, kullanıcının girdiği sayıdan küçükse, arama ortadaki değer + 1 ile dizinin son indeksi arasında devam eder. Bu adımlar, sayı bulunana kadar devam eder. +1 ve -1'in olmasının sebebi ilk başta ortadaki değeri kontrol ettiğimiz için tekrar kontrol etmemize gerek yoktur.

---
+++ DizidekiSayınınİndexiniBulma/DizidekiSayınınİndexiniBulma/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DizidekiSayınınİndexiniBulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dizi = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };");
+            int[] dizi = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            int sayı = Convert.ToInt32(Console.ReadLine());
+
+            int ilkİndex = 0;
+            int sonİndex = dizi.Length -1;
+            int index = -1;
+
+            while (ilkİndex <= sonİndex)
+            {
+                int orta = (ilkİndex + sonİndex) / 2;
+                if (dizi[orta] == sayı)
+                {
+                    index = orta;
+                    break;
+                }
+                else if (dizi[orta] < sayı)
+                {
+                    ilkİndex = orta + 1;
+                }
+                else
+                {
+                    sonİndex = orta - 1;    
+                }
+            }
+            if (index != -1)
+            {
+                Console.WriteLine($"Bulundu, indeks: {index}");
+            }
+            else
+            {
+                Console.WriteLine("Bulunamadı");
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
